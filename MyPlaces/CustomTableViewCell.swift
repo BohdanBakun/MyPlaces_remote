3f832651c3eafcf46ae407fa2498947c3317d047
Bohdan <hasps.bowler-0d@icloud.com>
2023-04-18 23:03:22 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Богдан Бакун on 18.04.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabek: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
