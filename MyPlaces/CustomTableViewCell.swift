f85463e6036779b874841f8cbb11c079eeb9a11a
Bohdan <hasps.bowler-0d@icloud.com>
2023-04-24 23:02:40 +0300

    New Features added

--- MyPlaces/CustomTableViewCell.swift
+++ MyPlaces/CustomTableViewCell.swift
@@ -11,7 +11,7 @@ class CustomTableViewCell: UITableViewCell {
 
     @IBOutlet var imageOfPlace: UIImageView!
     @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var locationLabek: UILabel!
+    @IBOutlet var locationLabel: UILabel!
     @IBOutlet var typeLabel: UILabel!
     
 }
